65000b3ed8a4fe06e85d26d71a86fad1eb0f3803
Rendy Anggara <rendyx942000@gmail.com>
2020-08-04 13:54:57 +0700

    fix name

--- database/migrations/2020_08_04_025740_create_user_reset_emails_table.php
+++ database/migrations/2020_08_04_025740_create_user_reset_emails_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateResetEmailsTable extends Migration
+class CreateUserResetEmailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateResetEmailsTable extends Migration
      */
     public function up()
     {
-        Schema::create('reset_emails', function (Blueprint $table) {
+        Schema::create('user_reset_emails', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
             $table->string('email');
@@ -31,6 +31,6 @@ class CreateResetEmailsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reset_emails');
+        Schema::dropIfExists('user_reset_emails');
     }
 }
--- database/migrations/2020_08_04_025755_create_user_reset_phones_table.php
+++ database/migrations/2020_08_04_025755_create_user_reset_phones_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateResetPhonesTable extends Migration
+class CreateUserResetPhonesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateResetPhonesTable extends Migration
      */
     public function up()
     {
-        Schema::create('reset_phones', function (Blueprint $table) {
+        Schema::create('user_reset_phones', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
             $table->string('phone');
@@ -31,6 +31,6 @@ class CreateResetPhonesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reset_phones');
+        Schema::dropIfExists('user_reset_phones');
     }
 }
