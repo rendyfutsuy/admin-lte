bf443a590f602aa172b6655c77c2ce0855b6c883
Rendy Anggara <rendyx942000@gmail.com>
2020-08-04 10:00:03 +0700

    create migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('banned_at')->nullable();
+            $table->integer('level')->default(1);
+            $table->boolean('is_banned')->default(0);
             $table->string('password');
+            $table->json('meta')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2020_08_04_025740_create_reset_emails_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResetEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reset_emails', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('email');
+            $table->string('activation_code');
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reset_emails');
+    }
+}
